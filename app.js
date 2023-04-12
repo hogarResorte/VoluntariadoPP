ff5276c850eae43798aa1ae52bcf05fe62ad39dd
Ornella Galli <ornegalli@gmail.com>
2023-04-12 13:57:27 -0300

    correccion errores de la consola

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function closePopup() {
 const iconoMenu = document.querySelector("#icono-menu"),
   menu = document.querySelector("#menu");
 
-iconoMenu.addEventListener("click", (e) => {
+iconoMenu.addEventListener('click', (e) => {
   menu.classList.toggle("active");
   document.body.classList.toggle("opacity");
 });
